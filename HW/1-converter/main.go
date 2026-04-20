69d8a4c8c8e450ce913b33a7e7735f19758ab984
Magomedov Murad <Magomedov93murad@gmail.com>
2026-04-20 14:57:35 +0300

    Home Work 4.12

--- HW/1-converter/main.go
+++ HW/1-converter/main.go
@@ -1,8 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
-	const usdTOEuro float64 = 0.85
-	const usdToRub float64 = 76.0
+	var usdTOEuro, usdToRub = usdTo()
 	var euroToRub = usdToRub / usdTOEuro
+
+	fmt.Printf("1 EUR = %.2f RUB\n", euroToRub)
+}
+
+func usdTo() (float64, float64) {
+	var euro float64
+	var rub float64
+	fmt.Println("Калькулятор валют")
+	fmt.Print("Введите курс USD к EUR: ")
+	fmt.Scan(&euro)
+
+	fmt.Print("Введите курс USD к RUB: ")
+	fmt.Scan(&rub)
+	return euro, rub
+}
+
+func convert(amount int, from float64, to float64) {
 
 }
